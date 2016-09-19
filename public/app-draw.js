55f84d1844cb164fd39341378bbe2f395601cc58
mikevance <tesla@Michaels-MBP.lan>
2016-09-19 19:14:02 -0400

    solo-randomizer

--- public/app-draw.js
+++ public/app-draw.js
@@ -4,21 +4,77 @@ var patternBlock;
 var current;
 
 function preload() {
-  patternBlock = loadImage("./face.png");
+  patternBlock = loadImage("./solo.png");
 
 }
 
 function setup() {
-  createCanvas(700,700);
+  createCanvas(windowWidth,windowHeight);
   background(255,255,255,255); //this is a color in RGBA colorspace : red, green, blue and alpha (opacity)
 }
 
-for(var x=0;x<8;x++){
-  function draw() {
-    background(244, 248, 252);
-    var u = 200;
-    image(patternBlock,mouseX,mouseY,100,100);
-    image(patternBlock,mouseX+u%2,mouseY+u,100,100);
-    u = u + 100;
+function draw(){
+  for(var x=0;x<windowWidth/10;x++){
+
+    for(var y=0;y<windowHeight/10;y++){
+      if(x%2==0){
+        var r = 45;
+        push();
+        translate(15+x*100, 15+y*115);
+        rotate(degrees(r));
+        translate();
+        image(patternBlock, 0, 0, 188, 200);
+        pop();
+      }
+      else {
+        var r = 45;
+        push();
+        translate(15+x*100, 15+y*115);
+        rotate(degrees(r));
+        translate();
+        image(patternBlock, 0, 0, 188, 200);
+        pop();
+      }
+    }
+  }
+
+  for(var x=0;x<8;x++){
+
+    for(var y=0;y<8;y++){
+      if(x%2==0){
+        var r = y*10+15;
+        var imageWidth = 188+y*2;
+        push();
+        translate(15+x*100, 15+y*115);
+        rotate(degrees(r));
+        translate();
+        image(patternBlock, 0, 0, imageWidth, imageWidth*1.06);
+        pop();
+        console.log(x)
+      }
+      else if(x%2==2){
+        var r = x*10+15;
+        var imageWidth = 188+x*10;
+        push();
+        translate(15+x*100, 15+y*115);
+        rotate(degrees(r));
+        translate();
+        image(patternBlock, 0, 0, imageWidth, imageWidth*1.06);
+        pop();
+      }
+      else if (x%2==4){
+        var r = x*20+15;
+        var imageWidth = 188+x*10;
+        push();
+        translate(15+x*100, 15+y*115);
+        rotate(degrees(r));
+        translate();
+        image(patternBlock, 0, 0, imageWidth, imageWidth*1.06);
+        pop();
+      }
+      else {
+        
+      }
+    }
   }
 }
---
+++ public/app-multi.js
@@ -0,0 +1,54 @@
+
+var patternBlock;
+var current;
+
+function preload() {
+  patternBlock = loadImage("./solo.png");
+}
+
+function setup() {
+  canvas_context=createCanvas(windowWidth,windowHeight);
+  background(176,176,99,0);
+  noLoop();
+}
+
+function draw(){
+  translate(mouseX);
+  for(var x=0;x<windowWidth/10;x++){
+      push();
+      translate(random(width), random(height));
+      rotate(random(2*PI));
+      scale(random(1.5,1.0,3.5));
+      makeCup();
+      pop();
+    }
+
+  makeInstructions();
+}
+
+function makeCup() {
+  image(patternBlock, 0, 0, 188, 200);
+}
+
+function makeInstructions() {
+  fill(0,0,0,255);
+  rect(0, 0, 250, 20)
+
+  fill(255, 255, 255, 255);
+  textSize(12)
+  textStyle(BOLD)
+  text("Press 'R' to generate new design!", 10, 15);
+}
+
+function keyPressed() {
+  if (keyCode === 82) {
+    windowResized();
+  } else if (keyCode === RIGHT_ARROW) {
+    console.log("Key: Right Arrow");
+  }
+}
+
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
